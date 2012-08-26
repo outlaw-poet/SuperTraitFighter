4b1c84e4cd0e2c97a69872dcd923afb4a2bee624
Justin Corwin <outlawpoet@gmail.com>
2012-08-26 00:02:37 -0700

    scenes for all major game points
    split up assets per scene, cutscene is visible

---
+++ SuperTraitFighterUnity/Assets/Behaviors/battleGameBehaviors.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+
+public class battleGameBehaviors : MonoBehaviour {
+	public XElement traitFile;
+	// Use this for initialization
+	void Start () {
+		traitFile = XElement.Load("TraitFile.xml");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void Awake(){
+		
+	}
+	
+	void OnGui(){
+	}
+	
+	public int compareArmyValue(ArmyTraits one, ArmyTraits two){
+		return 0;
+	}
+
+
+public class Trait{
+	public string code = "";
+	public string name = "";
+	public int Atk = 0;
+	public int Def = 0;
+	public int Agi = 0;
+	public Trait(int pAtk, int pDef, int pAgi, string pname, string pcode){
+		Atk = pAtk;
+		Def = pDef;
+		Agi = pAgi;
+		name = pname;
+		code = pcode;
+	}
+}
+
+public class ArmyTraits{
+	public List<Trait> traitCollection = new List<Trait>();
+	public ArmyTraits(string code, XElement traitFile){
+		for (int i = 0; i < (code.Length - 4) ; i += 4){
+			string codeTest = code.Substring(i, 4);
+			//what the fuck, load the goddamn variables from the xml file!!!
+			//it may be enum time.
+		}
+	}
+	public int Atk{
+		get {
+			return calcAtk();
+		}
+	}
+	int baseAtk = 20;
+	int calcAtk(){
+		int result = baseAtk;
+		foreach (Trait bleh in traitCollection){
+			result += bleh.Atk;
+		}
+		return result;
+	}
+	public int Def{
+		get {
+			return calcDef();
+		}
+	}
+	int baseDef = 20;
+	int calcDef(){
+		int result = baseDef;
+		foreach (Trait bleh in traitCollection){
+			result += bleh.Def;
+		}
+		return result;
+	}
+		public int Agi{
+		get {
+			return calcAgi();
+		}
+	}
+	int baseAgi = 20;
+	int calcAgi(){
+		int result = baseAgi;
+		foreach (Trait bleh in traitCollection){
+			result += bleh.Agi;
+		}
+		return result;
+	}
+}
+}
---
+++ SuperTraitFighterUnity/Assets/Behaviors/peaBehavior.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class peaBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
